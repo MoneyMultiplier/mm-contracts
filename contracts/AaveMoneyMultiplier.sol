b983211e29fddffd01f8ced76eeaa406ea40ea96
João Ferreira <joao.avf@gmail.com>
2021-10-22 19:28:43 +0100

    [CHECKPOINT] Working on deposit

--- contracts/AaveMoneyMultiplier.sol
+++ contracts/AaveMoneyMultiplier.sol
@@ -8,18 +8,29 @@ import "../interfaces/IAaveLendingPool.sol";
 contract AaveMoneyMultiplier {
 
     address _tokenAddress;
+    address _aTokenAddress;
     address _aaveLendingPoolAddress;
     IAaveLendingPool _aaveLendingPool;
     uint256 interestRateMode = 2;
 
     constructor(address tokenAddress) {
         _tokenAddress = tokenAddress;
+        _aTokenAddress = _aaveLendingPool.getReserveData(tokenAddress).aTokenAddress;
         _aaveLendingPool = ILendingPool(_aaveLendingPoolAddress);
         _aaveLendingPool.setUserUseReserveAsCollateral(_aaveLendingPoolAddress, true);
     }
 
     function deposit(uint256 amount) public view returns () {
-        _aaveLendingPool.deposit(_tokenAddress, amount, address(this), 0);
+        // Transfer Asset into contract
+        IERC20(inputs.tokens[i]).safeTransferFrom(msg.sender, address(this), amount);
+
+        // Lend Asset
+        _aaveLendingPool.deposit(_tokenAddress,
+                                 IERC20(_tokenAddress).balanceOf(address(this)),
+                                 address(this),
+                                 0);
+
+        // Borrow Asset
         _aaveLendingPool.borrow(_tokenAddress, amount, interestRateMode, 0, address(this));
         (,,uint256 availableBorrowsETH,,,uint256 healthFactor) = _aaveLendingPool.getUserAccountData();
     }
