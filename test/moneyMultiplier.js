b8cb5eee4ba437655268f32858848c2b4d636db9
João Ferreira <joao.avf@gmail.com>
2021-10-23 09:13:56 +0100

    Fixing addressProvider address

--- test/moneyMultiplier.js
+++ test/moneyMultiplier.js
@@ -3,7 +3,7 @@ const { ethers } = require("hardhat");
 
 describe("AaveMoneyMultiplier", function () {
   it("Should return the new greeting once it's changed", async function () {
-    const addressProvider = "0x52D306e36E3B6B02c153d0266ff0f85d18BCD413";
+    const addressProvider = "0x3ac4e9aa29940770aeC38fe853a4bbabb2dA9C19";
     const daiAddress = "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063";
 
     const MM = await ethers.getContractFactory("AaveMoneyMultiplier");
